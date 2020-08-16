f8a6383866c1470c0935e39fcb6c8cc3e4652b22
Cfomodz <ashby45@byui.edu>
2020-08-16 15:41:04 -0600

    made a headless version
    command line arg 1 = Title
    Everything else is just set as global vars for whatever the user normally uses (since frame rates don't often change from video to video).

---
+++ admaker_headless.py
@@ -0,0 +1,98 @@
+#Windows users: install ImageMagic & uncomment lines 2 & 3
+#from moviepy.config import change_settings
+#change_settings({"IMAGEMAGICK_BINARY": r"C:\\Program Files\\ImageMagick-7.0.10-Q16-HDRI\magick.exe"})
+
+from moviepy.editor import VideoFileClip, AudioFileClip, concatenate_videoclips, TextClip, CompositeVideoClip, concatenate_audioclips, AudioClip, CompositeAudioClip
+import os
+import sys
+
+HEADLESS = True
+arguments = len(sys.argv) - 1
+HEADLESS_FR = 24
+if arguments == 0:
+    HEADLESS_TITLE = ""
+else:
+    HEADLESS_TITLE = sys.argv[1]
+HEADLESS_ENDTEXT = "Thanks for Watching!"
+
+if not HEADLESS:
+    fr = input("Enter Desired Framerate, leave blank and hit enter for default(24): ")
+    if fr == "":
+        fr = 24
+    else: fr = int(fr)
+else: fr = HEADLESS_FR
+
+if not HEADLESS:
+    output_name = input("Enter Desired Output Name, leave blank and hit enter for default(final_video.mp4): ")
+    if output_name == "":
+        output_name = "final_video.mp4"
+else:
+    output_name = "final_video.mp4"
+    
+video_files = []
+
+#for each video in folder
+for i in os.listdir():
+    if i.endswith(".mp4"):
+        video_files.append(i)
+        
+#create 'clip' for each video
+for i, clip in enumerate(video_files):
+    video_files[i] = VideoFileClip(clip)
+
+print("Number of video files loaded: " + str(len(video_files)))
+
+#title frontend
+if not HEADLESS:
+    title = input("Enter Title Screen Text, leave blank and hit enter to skip: ")
+else:
+    title = HEADLESS_TITLE
+
+#endscreen
+if not HEADLESS:
+    endtext = input("Enter Endscreen Text, leave blank and hit enter to skip: ")
+else:
+    endtext = HEADLESS_ENDTEXT
+    if endtext != "":
+        video_files.append(TextClip(endtext, fontsize = 50, color = 'white').set_duration(5))
+    
+
+#combine video clips
+combined_clip = concatenate_videoclips(video_files, method="compose")
+
+#title backend
+if title != "":
+    txt_clip = TextClip(title,fontsize=70,color='white')
+    txt_clip = txt_clip.set_pos('bottom').set_duration(5)
+    video = CompositeVideoClip([combined_clip, txt_clip])
+else:
+    video = combined_clip
+    
+#audio
+audio_files = []
+
+for i in os.listdir():
+    if i.endswith(".mp3") or i.endswith(".wav"):
+        audio_files.append(i)
+
+print("Audio files loaded are: " + str(audio_files))
+
+for i, clip in enumerate(audio_files):
+    audio_files[i] = AudioFileClip(clip)
+
+#ToDo Concatenate audio tracks into audioclip
+combined_audio = concatenate_audioclips(audio_files)
+
+#Set Duration of audioclip
+background_audio = combined_audio.set_duration(video.duration)
+
+#combine videos' audio and audio track
+video_audio = video.audio
+#print(background_audio)
+#print(video_audio)
+final_audio = CompositeAudioClip([background_audio, video_audio])
+final_clip = video.set_audio(final_audio)
+
+#render
+print("Composition successful. Rendering!")
+final_clip.write_videofile(output_name, fps=fr, logger=None)
